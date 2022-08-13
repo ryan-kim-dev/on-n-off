644362c2b32bd629a2abe60ebf326fc8c7b67427
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-08-13 11:03:50 +0900

    feat: Implement Simple react app w/ test cases

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
+  return (
+    <div>
+      <header>
+        <h3 data-testid="counter">{count}</h3>
+        <div>
+          <button
+            data-testid="minus-button"
+            disabled={disabled}
+            onClick={() => setCount(prev => prev - 1)}
+          >
+            -
+          </button>
+          <button
+            data-testid="plus-button"
+            disabled={disabled}
+            onClick={() => setCount(prev => prev + 1)}
+          >
+            +
+          </button>
+        </div>
+        <div>
+          <button
+            data-testid="on/off-button"
+            style={{ backgroundColor: 'blue' }}
+            onCllick={() => setDisabled(prev => !prev)}
+          >
+            On/Off
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
